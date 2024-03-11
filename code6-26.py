14ebba3c2ce0c08277e239d8f0b7cd9372d0c3ac
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-03-11 16:47:58 +0900

    딕셔너리
    2024.3.11

---
+++ code6-26.py
@@ -0,0 +1,7 @@
+# 딕셔너리
+# 키 로 값을 추출한다 {key:값}
+stu1 = {"name":"손흥민", "year":1, "Credit":93, "talent":"축구"};
+stu2 = {"이름":"홍길동", "교양":50, "전공":60}
+print(stu1);
+print(stu1['name'], stu1['talent'], stu2["이름"], stu2["전공"])
+       
