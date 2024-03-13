67992eb7085fd380143058b3ae56c2e51f38922d
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-03-13 15:56:03 +0900

    정수 출력 예
    2024.3.13

---
+++ code7-9.py
@@ -0,0 +1,15 @@
+num1 = 82 ;  num2 = 2021 ;  num3 = -64 ;
+
+print("num1=%d , num2=%5d  num3=%5d" % (num1, num2, num3))
+print("num1=%+d , num2=%+5d  num3=%+5d" % (num1, num2, num3))
+print("num1=%-d , num2=%+-5d  num3=%-5d" % (num1, num2, num3))
+print("num1=%0d , num2=%+05d  num3=%+05d" % (num1, num2, num3))
+print("num1=%+-0d , num2=%+-05d  num3=%+-05d" % (num1, num2, num3))
+
+n1 = 1234 ;  n2 = -365
+
+print("%d  %d" % (n1, n2))
+print("%-6d  %-6d" % (n1, n2))
+print("%+6d  %+6d" % (n1, n2))
+print("10진수:%d 8진수:%o" % (n1, n1))
+print("16진수:%x 16진수:%X" % (n1, n1))
