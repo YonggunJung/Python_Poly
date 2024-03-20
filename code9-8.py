146d4c0cef106fe64f887525bbcacff787d60d6a
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-03-20 14:20:50 +0900

    최소 정수/최대 정수 및 평균 구하기
    2024.3.20

---
+++ code9-8.py
@@ -0,0 +1,20 @@
+print("최소 정수/최대 정수/평균 구하기(-1 : 입력 종료)")
+num = int(input("정수 입력 :"))
+if num != -1 :
+    n = 1;
+    sum = num
+    max = min = num
+    num = int(input("정수 입력 :"))
+    while num != -1:
+        n += 1
+        sum += num
+        if num < min:
+            min = num
+        elif num > max:
+            max = num
+        num = int(input("정수 입력 :"))
+    avg = sum / n
+    print("정수 개수: %d" % n)
+    print("최소 정수 : %d, 최대 정수 : %d" %(min, max))
+    print("입력된 정수 평균 : %.2f" % avg)
+print("프로그램 종")
