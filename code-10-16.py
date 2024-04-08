e0be4dfc2b84a8e5205d292c7a472370fe89ed33
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-04-08 15:35:25 +0900

    재귀함수 하노이타워
    2024.4.8

---
+++ code-10-16.py
@@ -0,0 +1,13 @@
+def  hanoiTower(n, a, b, t) :
+    if n>=1 :
+        hanoiTower(n-1, a, t, b)
+        print(n, '-1',a, t, b)
+        print(" %d-원반 이동  : %c->%c \n" % (n, a, b))
+        hanoiTower(n-1, t, b, a)
+        print(n, '-2',t, a, b)
+    else :
+        pass
+    return
+
+n = int(input("[하노이 타워 문제] 원반 개수: "))
+hanoiTower(n, 'A', 'B', 'T')
