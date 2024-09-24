35d94ac1268cae39e6fda6b47ab4b2b0a47572be
CodeRobin <119474459+YonggunJung@users.noreply.github.com>
2024-09-24 12:52:11 +0900

    2024.9.24

---
+++ 자료구조3-1.py
@@ -0,0 +1,7 @@
+px = [7, -4, 0, 5]
+polyStr = "P(x) = "
+polyStr += " + " + str(px[0]) + "x^" + str(3)
+polyStr += " + " + str(px[1]) + "x^" + str(2)
+polyStr += " + " + str(px[2]) + "x^" + str(1)
+polyStr += " + " + str(px[3]) + "x^" + str(0)
+print(polyStr)
